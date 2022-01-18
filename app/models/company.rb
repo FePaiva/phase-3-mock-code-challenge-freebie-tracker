acf1c001d11a9ccdfc363b1a34d61a13f4298d09
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-01-18 14:19:40 -0800

    done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,20 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def give_freebie(dev:, item_name:, value:)
+      Freebie.create(item_name: item_name, value: value, dev_id: dev.id, company_id: self.id)
+  
+    # to check if give_freebie works: open console, create a dev => dev = Dev.first,
+    # then create a company => c = Company.first, 
+    # then invoke the method => c.give_freebie(dev: dev, item_name: "Item 5", value: 35)
+    #  check if there is a new entry in the freebies table (sqlite tab -> freebies).
+
+  end
+
+  def self.oldest_company
+     year = self.minimum(:founding_year)
+      self.find_by(founding_year: year)
+  end
+# to check if oldest_company works: open console, type Company.oldest_company . 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,11 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "Item 1", value: 5, dev_id: 1, company_id: 1)
+Freebie.create(item_name: "Item 2", value: 15, dev_id: 1, company_id: 2)
+Freebie.create(item_name: "Item 3", value: 25, dev_id: 2, company_id: 1)
+Freebie.create(item_name: "Item 4", value: 50, dev_id: 2, company_id: 2)
+
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
