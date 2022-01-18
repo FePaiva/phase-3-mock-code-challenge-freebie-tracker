ae16436ebb1ba97f1119a3b17faba49902d57292
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-01-18 14:17:24 -0800

    commit tree 8bbe450afcfd29412be4a918a2249ab9c3b2402e

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,28 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
-end
+  def received_one?(item_name)
+  #     if self.freebies.find_by(item_name: item_name) == nil
+  #       false
+  #     else 
+  #       true
+  #     end
+  # end
+
+  self.freebies.find_by(item_name: item_name) == nil ? false : true
+  end
+  # to check if received_one? works: open console, create a dev => dev = Dev.first, 
+  # then check the formula => dev.received_one?("Item 1") . 
+
+  def give_away(dev:, freebie:)
+      freebie.dev_id = dev.id
+      freebie.save
+  end
+# to check if give_away works: open console, get a dev => dev = Dev.find(4),
+# then get a freebie => f = Freebie.first , 
+# then check the formula dev.give_away(dev: dev, freebie: f).
+# if u check the freebies table, the dev_id 1 changed to dev_id 4.
+
+end  
+
--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,11 @@
 class Freebie < ActiveRecord::Base
+  belongs_to :dev
+  belongs_to :company
+
+  def print_details
+    puts "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
+  end
+# to check if received_one? works: open console, create a freebie => f = Freebie.first
+  # then check the formula => f.print_details
 
 end
